b15d55470d0f78e35dd557e5f061408b700938f2
kshitiznigam <kshitiz.nigam280@gmail.com>
2023-08-23 22:14:11 +0530

    Update app.service.ts

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -7,30 +7,27 @@ import { Customer } from '../customer/customer.component';
   providedIn: 'root'
 })
 export class ApiService {
-  public apiUrl = "https://reqres.in/api/users";
-
+  public apiUrl = "http://localhost:5148/api/Customer"; // Update the API URL according to the route in CustomerController.cs
 
   constructor(private http: HttpClient) { }
 
   getCustomers(): Observable<Customer[]> {
-    return this.http.get<Customer[]>(`${this.apiUrl}`);
-
+    return this.http.get<Customer[]>(`${this.apiUrl}/GetCustomers`);
   }
 
   addCustomer(customer: Customer): Observable<Customer> {
-    return this.http.post<Customer>(`${this.apiUrl}/customers`, customer);
+    return this.http.post<Customer>(`${this.apiUrl}/AddCustomer`, customer);
   }
 
   editCustomer(customer: Customer): Observable<Customer> {
-    return this.http.put<Customer>(`${this.apiUrl}/customers/${customer.customerId}`, customer);
+    return this.http.put<Customer>(`${this.apiUrl}/EditCustomer/${customer.customerId}`, customer);
   }
 
   deleteCustomer(customerId: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/customers/${customerId}`);
+    return this.http.delete<void>(`${this.apiUrl}/DeleteCustomer/${customerId}`);
   }
 
   submitFormData(formData: any, headers: any): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/submit`, formData);
-    
   }
 }
